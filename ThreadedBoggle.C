129877ace51cbc19d7ebda7dd8ae44a996e22aa5
cindyanorris <can@cs.appstate.edu>
2021-07-19 11:21:28 -0400

    modified comments

--- ThreadedBoggle.C
+++ ThreadedBoggle.C
@@ -14,11 +14,18 @@
  */
 ThreadedBoggle::ThreadedBoggle(BoggleBoard * board, int threadPoolSize):Boggle(board)
 {
-  //initialize the thread pool and the bestWord
+  //Initialize the thread pool and the bestWord. A pointer
+  //to a ThreadPool object and an atomic bestWord are declared in
+  //ThreadedBoggle.h.
+  //
+  //You can initialize an atomic with a store.  Google it.
+  //
   //The ThreadPool constructor needs to be passed the number of
   //threads in the pool (threadPoolSize) and the number of
   //tasks that are going to created (the number of times spawn
-  //will be called).  This second parameter is needed to prevent
+  //will be called -- 16 for this program). 
+  //
+  //Note: this second parameter is needed to prevent
   //a race condition.  There's an error in the book code where
   //a thread might set stop_pool to true when the main thread still
   //has tasks to add.
@@ -42,12 +49,13 @@ void ThreadedBoggle::updateSolution(std::string newWord)
  */
 void ThreadedBoggle::updateBestWord(std::string newWord)
 {
-  //Get the up to 8 character that make up the bestWord
+  //Get the up to 8 characters that make up the bestWord
   //out of the 64 bit atomic variable
   //You'll need to add a declaration of the atomic variable to
   //ThreadedBoggle.h
   //
-    //convert the 8 characters into a string
+    //convert the 8 characters into a string (Write a method or
+    //lambda expression to do this.)
 
     //compare the current best word string to the newWord string
     //to see if the best word should be updated to the newWord
